f3bfb62d3381c26a5bdcf8df7a7e411c42eb1914
siddiksaurav <siddiquesaurav@gmail.com>
2023-12-26 04:42:44 +0600

    updated CategoryRepository

--- src/main/java/com/farmfresh/marketplace/OrchardCart/repository/CategoryRepository.java
+++ src/main/java/com/farmfresh/marketplace/OrchardCart/repository/CategoryRepository.java
@@ -10,4 +10,6 @@ import java.util.Optional;
 @Repository
 public interface CategoryRepository extends JpaRepository<Category,Long> {
     Optional<Category> findById(Long id);
+
+    Category findByCategoryName(String categoryName);
 }
