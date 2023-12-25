af65c858b127c7109cdf328b9eb0ba7aad1e233c
siddiksaurav <siddiquesaurav@gmail.com>
2023-12-26 04:46:44 +0600

    Updated product service class to add business logic

--- src/main/java/com/farmfresh/marketplace/OrchardCart/service/ProductService.java
+++ src/main/java/com/farmfresh/marketplace/OrchardCart/service/ProductService.java
@@ -1,26 +1,49 @@
 package com.farmfresh.marketplace.OrchardCart.service;
 
+import com.farmfresh.marketplace.OrchardCart.dto.ProductRequest;
 import com.farmfresh.marketplace.OrchardCart.model.Category;
 import com.farmfresh.marketplace.OrchardCart.model.Product;
+import com.farmfresh.marketplace.OrchardCart.model.Seller;
+import com.farmfresh.marketplace.OrchardCart.repository.CategoryRepository;
 import com.farmfresh.marketplace.OrchardCart.repository.ProductRepository;
+import com.farmfresh.marketplace.OrchardCart.repository.SellerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.nio.file.AccessDeniedException;
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
 public class ProductService {
     @Autowired
     ProductRepository productRepository;
+    @Autowired
+    SellerRepository sellerRepository;
+    @Autowired
+    CategoryRepository categoryRepository;
 
     public List<Product> getProductList() {
         List<Product> lists = productRepository.findAll();
         return lists;
     }
 
-    public String addProduct(Product product) {
+    @Transactional
+    public String addProduct(ProductRequest productRequest) {
+        Seller seller = sellerRepository.findByBusinessName(productRequest.getBusinessName());
+        Category category = categoryRepository.findByCategoryName(productRequest.getCategoryName());
+        Product product = new Product();
+        product.setName(productRequest.getName());
+        product.setDescription(productRequest.getDescription());
+        product.setPrice(productRequest.getPrice());
+        product.setQuantity(productRequest.getQuantity());
+        product.setCategory(category);
+        product.setSeller(seller);
+        category.getProducts().add(product);
+        categoryRepository.save(category);
         productRepository.save(product);
         return "success";
     }
@@ -35,18 +58,41 @@ public class ProductService {
         }
     }
 
-    public Product updateProductById(Long id, Product product) {
+    @Transactional
+    public Product updateProductById(Long id, ProductRequest product) {
+        Seller seller = sellerRepository.findByBusinessName(product.getBusinessName());
+        Category category = categoryRepository.findByCategoryName(product.getCategoryName());
         Product existingProduct = productRepository.findById(id).orElseThrow(() -> new NoSuchElementException("Product not found"));
+
+        if (!Objects.equals(existingProduct.getSeller(), seller)) {
+            try {
+                throw new AccessDeniedException("You are not authorized to update this product");
+            } catch (AccessDeniedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        if (!Objects.equals(existingProduct.getCategory(), category)) {
+            Category previousCategory = existingProduct.getCategory();
+            previousCategory.getProducts().remove(existingProduct);
+            existingProduct.setCategory(category);
+            category.getProducts().add(existingProduct);
+            categoryRepository.save(category);
+            categoryRepository.save(previousCategory);
+        }
         existingProduct.setName(product.getName());
         existingProduct.setDescription(product.getDescription());
         existingProduct.setQuantity(product.getQuantity());
         existingProduct.setPrice(product.getPrice());
-        existingProduct.setCategory(product.getCategory());
+        existingProduct.setSeller(seller);
+
         productRepository.save(existingProduct);
+
         return existingProduct;
     }
 
+    /*
     public Optional<Product> getProductByCategoryId(Long categoryId) {
         return productRepository.findByCategoryId(categoryId);
-    }
+    }*/
 }
