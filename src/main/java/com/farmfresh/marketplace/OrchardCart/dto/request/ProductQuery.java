c4cc1414c4a274396d210533c0c8bf8d19551222
siddiksaurav <siddiquesaurav@gmail.com>
2024-01-02 05:48:33 +0600

    Added Product query request class

---
+++ src/main/java/com/farmfresh/marketplace/OrchardCart/dto/request/ProductQuery.java
@@ -0,0 +1,14 @@
+package com.farmfresh.marketplace.OrchardCart.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class ProductQuery {
+   @Email(message = "Invalid email address")
+   private String userEmail;
+   @NotBlank(message = "product name required")
+   private String productName;
+   private String message;
+}
