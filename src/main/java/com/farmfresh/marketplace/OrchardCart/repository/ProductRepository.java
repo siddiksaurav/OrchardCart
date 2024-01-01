5b0f33533680cafe91c0d6370082013b8e94a8b8
siddiksaurav <siddiquesaurav@gmail.com>
2024-01-02 05:50:31 +0600

    Updated product repository

--- src/main/java/com/farmfresh/marketplace/OrchardCart/repository/ProductRepository.java
+++ src/main/java/com/farmfresh/marketplace/OrchardCart/repository/ProductRepository.java
@@ -3,6 +3,8 @@ package com.farmfresh.marketplace.OrchardCart.repository;
 import com.farmfresh.marketplace.OrchardCart.model.Category;
 import com.farmfresh.marketplace.OrchardCart.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,4 +12,6 @@ import java.util.Optional;
 @Repository
 public interface ProductRepository extends JpaRepository<Product,Long> {
     Optional<Product> findByCategoryId(Long category_id);
+    @Query("SELECT p.seller.userInfo.email FROM Product p WHERE p.id = :productId")
+    String findSellerEmailByProductId(@Param("productId") Long productId);
 }
