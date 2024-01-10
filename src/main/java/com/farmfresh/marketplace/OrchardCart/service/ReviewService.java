ff77c7eee5a6b7199cfcbd334b7fdac3abec0894
siddiksaurav <siddiquesaurav@gmail.com>
2024-01-10 13:39:34 +0600

    Added review service class

---
+++ src/main/java/com/farmfresh/marketplace/OrchardCart/service/ReviewService.java
@@ -0,0 +1,39 @@
+package com.farmfresh.marketplace.OrchardCart.service;
+
+import com.farmfresh.marketplace.OrchardCart.dto.request.RatingRequest;
+import com.farmfresh.marketplace.OrchardCart.dto.request.ReviewRequest;
+import com.farmfresh.marketplace.OrchardCart.exception.ElementNotFoundException;
+import com.farmfresh.marketplace.OrchardCart.model.Product;
+import com.farmfresh.marketplace.OrchardCart.model.Rating;
+import com.farmfresh.marketplace.OrchardCart.model.Review;
+import com.farmfresh.marketplace.OrchardCart.model.UserInfo;
+import com.farmfresh.marketplace.OrchardCart.repository.ProductRepository;
+import com.farmfresh.marketplace.OrchardCart.repository.RatingRepository;
+import com.farmfresh.marketplace.OrchardCart.repository.ReviewRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ReviewService {
+    private final ReviewRepository reviewRepository;
+    private final ProductRepository productRepository;
+    private final JwtService jwtService;
+    public Review createReview(ReviewRequest reviewRequest, String userToken) throws ElementNotFoundException {
+        Product product = productRepository.findById(reviewRequest.getProductId()).orElseThrow(()->new ElementNotFoundException("product not found with id:"+reviewRequest.getProductId()));
+        UserInfo user = jwtService.getUserByToken(userToken);
+        Review review =new Review();
+        review.setProduct(product);
+        review.setReview(reviewRequest.getReview());
+        review.setUser(user);
+        review.setCreateTime(LocalDateTime.now());
+        return  reviewRepository.save(review);
+    }
+
+    public List<Review> getProductReviews(Integer productId){
+        return reviewRepository.findReviewsByProductId(productId);
+    }
+}
