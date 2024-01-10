afbf27a157efa3195f149e7533f55ffc1a40d896
siddiksaurav <siddiquesaurav@gmail.com>
2024-01-10 13:37:50 +0600

    Added cart sevice classes

---
+++ src/main/java/com/farmfresh/marketplace/OrchardCart/service/CartItemService.java
@@ -0,0 +1,52 @@
+package com.farmfresh.marketplace.OrchardCart.service;
+
+import com.farmfresh.marketplace.OrchardCart.dto.request.CartItemRequest;
+import com.farmfresh.marketplace.OrchardCart.exception.ElementNotFoundException;
+import com.farmfresh.marketplace.OrchardCart.model.Cart;
+import com.farmfresh.marketplace.OrchardCart.model.CartItem;
+import com.farmfresh.marketplace.OrchardCart.model.Product;
+import com.farmfresh.marketplace.OrchardCart.model.UserInfo;
+import com.farmfresh.marketplace.OrchardCart.repository.CartItemRepository;
+import com.farmfresh.marketplace.OrchardCart.repository.UserInfoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CartItemService {
+    @Autowired
+    private CartItemRepository cartItemRepository;
+    public void createCartItem(CartItem cartItem){
+        cartItemRepository.save(cartItem);
+    }
+    public CartItem updateCartItem(Integer userId,Integer cartItemId, CartItem cartItem) throws ElementNotFoundException {
+        CartItem existingItem = cartItemRepository.findById(cartItemId).orElseThrow(()-> new ElementNotFoundException("CartItem nitfound with Id:"+cartItemId));
+        if(userId.equals(existingItem.getUserId()))
+        {
+            existingItem.setQuantity(cartItem.getQuantity());
+            existingItem.setPrice(cartItem.getProduct().getPrice()*cartItem.getQuantity());
+
+        }
+        return cartItemRepository.save(existingItem);
+    }
+    public CartItem isCartItemExist(Cart cart, Product product, Integer userId){
+        CartItem cartItem = cartItemRepository.isCartItemExist(cart,product,userId);
+        return  cartItem;
+    }
+
+    public void removeCartItem(Integer userId,Integer cartItemId) throws AccessDeniedException, ElementNotFoundException {
+        CartItem cartItem = findCartItemById(cartItemId);
+        if(userId.equals(cartItem.getUserId())){
+            cartItemRepository.deleteById(cartItemId);
+        }
+        else {
+            throw new AccessDeniedException("You are not Authorized to remove cartitem");
+        }
+    }
+    public CartItem findCartItemById(Integer cartItemId) throws ElementNotFoundException {
+        return cartItemRepository.findById(cartItemId).orElseThrow(()->new ElementNotFoundException("cart item not available with id :"+cartItemId));
+    }
+
+}
---
+++ src/main/java/com/farmfresh/marketplace/OrchardCart/service/CartService.java
@@ -0,0 +1,61 @@
+package com.farmfresh.marketplace.OrchardCart.service;
+
+import com.farmfresh.marketplace.OrchardCart.dto.request.CartItemRequest;
+import com.farmfresh.marketplace.OrchardCart.dto.response.ProductResponse;
+import com.farmfresh.marketplace.OrchardCart.exception.ElementNotFoundException;
+import com.farmfresh.marketplace.OrchardCart.model.Cart;
+import com.farmfresh.marketplace.OrchardCart.model.CartItem;
+import com.farmfresh.marketplace.OrchardCart.model.Product;
+import com.farmfresh.marketplace.OrchardCart.model.UserInfo;
+import com.farmfresh.marketplace.OrchardCart.repository.CartRepository;
+import com.farmfresh.marketplace.OrchardCart.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Optional;
+
+@Service
+public class CartService {
+    @Autowired
+    private CartRepository cartRepository;
+    @Autowired
+    private ProductRepository productRepository;
+    @Autowired
+    private  CartItemService cartItemService;
+    public Cart creatCart(UserInfo user) {
+        Cart cart = new Cart();
+        cart.setUserInfo(user);
+        return  cartRepository.save(cart);
+    }
+
+    @PostMapping("/add")
+    public String addCartItem(Integer userId, CartItemRequest cartItemRequest) throws ElementNotFoundException {
+        Cart cart = cartRepository.findCartByUserInfoId(userId);
+        Product product = productRepository.findById(cartItemRequest.getProductId()).orElseThrow(()->new ElementNotFoundException("Product not found with Id:"+cartItemRequest.getProductId()));
+        CartItem isExist = cartItemService.isCartItemExist(cart,product,userId);
+        if (isExist == null) {
+            CartItem cartItem = new CartItem();
+            cartItem.setCart(cart);
+            cartItem.setProduct(product);
+            cartItem.setQuantity(cartItemRequest.getQuantity());
+            cartItem.setPrice(cartItemRequest.getQuantity() * product.getPrice());
+            cartItem.setUserId(userId);
+            cartItemService.createCartItem(cartItem);
+            cart.getCartItems().add(cartItem);
+        }
+        return "Added cart item successfully";
+    }
+    public Cart findUserCart(Integer userId) {
+        Cart cart = cartRepository.findCartByUserInfoId(userId);
+        double totalPrice = 0;
+        int totalItem = 0;
+        for (CartItem item : cart.getCartItems()) {
+            totalPrice = totalPrice + item.getPrice();
+            totalItem= totalItem + item.getQuantity();
+        }
+        cart.setTotalItem(totalItem);
+        cart.setTotalPrice(totalPrice);
+        return cartRepository.save(cart);
+    }
+}
