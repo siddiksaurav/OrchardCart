dbf0afe968ad9a0f5eb8d1a49c4efeb866736868
siddiksaurav <siddiquesaurav@gmail.com>
2024-01-17 10:02:59 +0600

    Updated jwt flter to get token from cookie

--- src/main/java/com/farmfresh/marketplace/OrchardCart/config/JwtAuthenticationFilter.java
+++ src/main/java/com/farmfresh/marketplace/OrchardCart/config/JwtAuthenticationFilter.java
@@ -3,9 +3,12 @@ package com.farmfresh.marketplace.OrchardCart.config;
 import com.farmfresh.marketplace.OrchardCart.service.JwtService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -16,26 +19,40 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final JwtService jwtService;
     private final UserDetailsService userDetailsService;
+    Logger log = LoggerFactory.getLogger(JwtAuthenticationFilter.class);
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         if (request.getServletPath().contains("/auth")) {
             filterChain.doFilter(request, response);
             return;
         }
-        final String authHeader = request.getHeader("Authorization");
-        final String jwt;
+
         final String userEmail;
-        if (authHeader == null ||!authHeader.startsWith("Bearer ")) {
+        Cookie[] cookies = request.getCookies();
+        String jwt=null;
+
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if ("Bearer".equals(cookie.getName())) {
+                    jwt = cookie.getValue();
+                    break;
+                }
+            }
+        }
+
+        /*if (authHeader == null ||!authHeader.startsWith("Bearer ")) {
             filterChain.doFilter(request, response);
             return;
         }
         jwt = authHeader.substring(7);
+        */
         userEmail = jwtService.extractUsername(jwt);
         if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
             UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
