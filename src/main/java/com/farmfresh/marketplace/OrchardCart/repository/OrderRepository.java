4ce93250bd5aeac24b0209c93fcee1b5b5327ea4
siddiksaurav <siddiquesaurav@gmail.com>
2024-01-19 20:11:51 +0600

    Updated order repository

--- src/main/java/com/farmfresh/marketplace/OrchardCart/repository/OrderRepository.java
+++ src/main/java/com/farmfresh/marketplace/OrchardCart/repository/OrderRepository.java
@@ -1,6 +1,5 @@
 package com.farmfresh.marketplace.OrchardCart.repository;
-
-import com.farmfresh.marketplace.OrchardCart.model.Order;
+import com.farmfresh.marketplace.OrchardCart.model.Orders;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -8,6 +7,6 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface OrderRepository extends JpaRepository<Order,Integer> {
-    List<Order> findByUserId(Integer userId);
+public interface OrderRepository extends JpaRepository<Orders,Integer> {
+    List<Orders> findByUserId(Integer userId);
 }
