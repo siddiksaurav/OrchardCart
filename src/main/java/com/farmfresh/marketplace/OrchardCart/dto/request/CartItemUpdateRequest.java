f9a39fcb89e2be4359e59b7e480067d033e7422f
siddiksaurav <siddiquesaurav@gmail.com>
2024-01-19 20:06:16 +0600

    Added cartItem update request class

---
+++ src/main/java/com/farmfresh/marketplace/OrchardCart/dto/request/CartItemUpdateRequest.java
@@ -0,0 +1,22 @@
+package com.farmfresh.marketplace.OrchardCart.dto.request;
+
+public class CartItemUpdateRequest {
+    private Integer productId;
+    private Integer quantity;
+
+    public Integer getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Integer productId) {
+        this.productId = productId;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+}
